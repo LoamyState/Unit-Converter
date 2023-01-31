c64664a89af9683cb875edf13694493df129a711
Jane Madsen <janemadsen815@gmail.com>
2023-01-31 10:15:23 -0700

    Initial Commit

---
+++ Unit Converter/Unit_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_ConverterApp.swift
+//  Unit Converter
+//
+//  Created by Jane Madsen on 1/31/23.
+//
+
+import SwiftUI
+
+@main
+struct Unit_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
