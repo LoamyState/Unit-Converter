f9a4a1fbe0dd2bed4fb43667178b6674c8d3c598
Jane Madsen <janemadsen815@gmail.com>
2023-05-18 07:42:57 -0600

    Finish unit converter

--- Unit Converter/ContentView.swift
+++ Unit Converter/ContentView.swift
@@ -8,15 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+    enum TimeUnit: CaseIterable {
+        case seconds, minutes, hours, days
     }
+    
+    @State private var inputUnitSelected: TimeUnit = .seconds
+    @State private var outputUnitSelected: TimeUnit = .minutes
+    @State private var valueToConvert: Double? = nil
+        
+    var body: some View {
+        Form {
+            Section {
+                TextField("Input Value", value: $valueToConvert, format: .number)
+                Picker("Select Unit", selection: $inputUnitSelected) {
+                    ForEach(TimeUnit.allCases.map { $0 }, id:\.self) {
+                        Text("\($0)".capitalized)
+                    }
+                }
+                .pickerStyle(.segmented)
+            } header: {
+                Text("Value to Convert")
+            }
+            Section {
+                Text("Output Value: \(convertValue().formatted())")
+                Picker("Select Unit", selection: $outputUnitSelected) {
+                    ForEach(TimeUnit.allCases.map { $0 }, id:\.self) {
+                        Text("\($0)".capitalized)
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+        }
+    }
+    
+    func convertValue() -> Double {
+        guard let valueToConvert = valueToConvert else {
+            return 0
+        }
+        
+        var inputValueInSeconds: Double {
+            switch inputUnitSelected {
+            case .seconds:
+                return valueToConvert
+            case .minutes:
+                return valueToConvert * 60
+            case .hours:
+                return valueToConvert * 60 * 60
+            case .days:
+                return valueToConvert * 24 * 60 * 60
+            }
+            
+        }
+        
+        switch outputUnitSelected {
+        case .seconds:
+            return inputValueInSeconds
+        case .minutes:
+            return inputValueInSeconds / 60
+        case .hours:
+            return inputValueInSeconds / 60 / 60
+        case .days:
+            return inputValueInSeconds / 24 / 60 / 60
+        }
+        
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
